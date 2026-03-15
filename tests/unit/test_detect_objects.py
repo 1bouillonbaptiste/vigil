eddb184c2683d43f46d2f821dcc53697b3c44318
1bouillonbaptiste <baptiste.bouillon@safran-ai.safrangroup.com>
2026-03-15 18:15:43 +0100

    refactor: testing context.

--- tests/unit/test_detect_objects.py
+++ tests/unit/test_detect_objects.py
@@ -1,22 +1,38 @@
-from uuid import UUID, uuid4
-from dataclasses import replace
+from dataclasses import dataclass
+from uuid import UUID
+
+import pytest
 
 from vigil.adapters.secondary.in_memory_detection_repository import InMemoryDetectionRepository
-from vigil.business_logic.models.detection import Detection, BoundingBox
+from vigil.business_logic.models.detection import BoundingBox, Detection
 from vigil.business_logic.use_cases.detect_objects import DetectObjectsUseCase
 
 
-def test_should_detect_a_person():
-    # Given
+@dataclass
+class ThisContext:
+    """Context for testing `DetectObjectsUseCase`."""
+
+    detection_repository: InMemoryDetectionRepository
+    use_case: DetectObjectsUseCase
+
+
+@pytest.fixture
+def this_context() -> ThisContext:
     detection_repository = InMemoryDetectionRepository()
     use_case = DetectObjectsUseCase(detection_repository=detection_repository)
+    return ThisContext(detection_repository=detection_repository, use_case=use_case)
 
+
+def test_should_detect_a_person(this_context: ThisContext):
+    # Given
 
     # When
-    use_case.execute()
+    this_context.use_case.execute()
 
     # Then
-    detections = detection_repository.get_by_video_id(video_id=UUID("9022e4bf-4ff8-4381-8dcd-b8dd588325cb"))
+    detections = this_context.detection_repository.get_by_video_id(
+        video_id=UUID("9022e4bf-4ff8-4381-8dcd-b8dd588325cb")
+    )
     assert len(detections) == 1
     assert detections[0] == Detection(
         id=detections[0].id,
@@ -25,4 +41,3 @@ def test_should_detect_a_person():
         bbox=BoundingBox(center_x=0, center_y=0, width=1, height=1),
         confidence=0.5,
     )
-
--- vigil/business_logic/use_cases/detect_objects.py
+++ vigil/business_logic/use_cases/detect_objects.py
@@ -1,7 +1,7 @@
 from uuid import UUID
 
 from vigil.business_logic.gateways.detection_repository import DetectionRepository
-from vigil.business_logic.models.detection import Detection, BoundingBox
+from vigil.business_logic.models.detection import BoundingBox, Detection
 
 
 class DetectObjectsUseCase:
@@ -12,10 +12,12 @@ class DetectObjectsUseCase:
 
     def execute(self):
         """Execute the use case on a single frame."""
-        self._detection_repository.save(Detection(
-            id=UUID("6f7f36e7-c0c8-4679-b3c3-835fc20ca59b"),
-            video_id=UUID("9022e4bf-4ff8-4381-8dcd-b8dd588325cb"),
-            frame_index=0,
-            bbox=BoundingBox(center_x=0, center_y=0, width=1, height=1),
-            confidence=0.5,
-        ))
+        self._detection_repository.save(
+            Detection(
+                id=UUID("6f7f36e7-c0c8-4679-b3c3-835fc20ca59b"),
+                video_id=UUID("9022e4bf-4ff8-4381-8dcd-b8dd588325cb"),
+                frame_index=0,
+                bbox=BoundingBox(center_x=0, center_y=0, width=1, height=1),
+                confidence=0.5,
+            )
+        )
