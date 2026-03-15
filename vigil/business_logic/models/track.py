6c5d9b305cf3920c16e558c97e1503e1e5253b35
1bouillonbaptiste <baptiste.bouillon@safran-ai.safrangroup.com>
2026-03-15 14:21:22 +0100

    refactor: Track owns its thumbnail.

--- vigil/business_logic/models/track.py
+++ vigil/business_logic/models/track.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
-from uuid import UUID
+from uuid import UUID, uuid4
+
+from vigil.business_logic.models.detection import Detection
 
 
 @dataclass(frozen=True)
@@ -18,6 +20,21 @@ class Track:
     thumbnail_id: UUID
     """Identifier of the most representative detection in the track."""
 
+    @classmethod
+    def create(cls, video_id: UUID, detections: list[Detection]) -> "Track":
+        """Creates a new track instance."""
+        return Track(
+            id=uuid4(),
+            video_id=video_id,
+            detections=[detection.id for detection in detections],
+            thumbnail_id=_get_most_representative(detections).id,
+        )
+
     def is_valid(self) -> bool:
         """Check if the track has enough detections to be tracked."""
         return len(self.detections) > 4
+
+
+def _get_most_representative(detections: list[Detection]) -> Detection:
+    """Find the most representative detection, first one by default."""
+    return max(detections, key=lambda detection: detection.score())
--- vigil/business_logic/use_cases/track_objects.py
+++ vigil/business_logic/use_cases/track_objects.py
@@ -1,10 +1,8 @@
-import uuid
 from uuid import UUID
 
 from vigil.business_logic.gateways.detection_repository import DetectionRepository
 from vigil.business_logic.gateways.track_repository import TrackRepository
 from vigil.business_logic.gateways.tracker import Tracker
-from vigil.business_logic.models.detection import Detection
 from vigil.business_logic.models.track import Track
 
 
@@ -23,16 +21,6 @@ class TrackObjectsUseCase:
         for instance_detections in instances:
             if not instance_detections:
                 continue
-            new_track = Track(
-                id=uuid.uuid4(),
-                video_id=video_id,
-                detections=[detection.id for detection in instance_detections],
-                thumbnail_id=_get_most_representative(instance_detections).id,
-            )
+            new_track = Track.create(video_id=video_id, detections=instance_detections)
             if new_track.is_valid():
                 self._tracks_repository.save(new_track)
-
-
-def _get_most_representative(detections: list[Detection]) -> Detection:
-    """Find the most representative detection, first one by default."""
-    return max(detections, key=lambda detection: detection.score())
