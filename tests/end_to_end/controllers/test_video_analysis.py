8e76c176632aa1df72b93bb768c3960edd1dd471
1bouillonbaptiste <1bouillonbaptiste@gmail.com>
2026-03-21 20:30:09 +0100

    feat: include video analysis workflow.

--- tests/end_to_end/controllers/test_video_analysis.py
+++ tests/end_to_end/controllers/test_video_analysis.py
@@ -5,7 +5,17 @@ import numpy as np
 import pytest
 from starlette.testclient import TestClient
 
-from vigil.adapters.primary.fastapi.app_dependencies import get_video_repository
+from vigil.adapters.primary.fastapi.app_dependencies import (
+    _get_detection_model,
+    _get_frame_repository,
+    _get_track_repository,
+    _get_tracker,
+    _get_video_repository,
+)
+from vigil.adapters.secondary.fake_detection_model import FakeDetectionModel
+from vigil.adapters.secondary.fake_tracker import FakeTracker
+from vigil.adapters.secondary.in_memory_frame_repository import InMemoryFrameRepository
+from vigil.adapters.secondary.in_memory_track_repository import InMemoryTrackRepository
 from vigil.adapters.secondary.local_video_repository import LocalVideoRepository
 from vigil.business_logic.models.video_source import VideoSource
 from vigil.business_logic.services.id_factory import IdFactory
@@ -27,9 +37,18 @@ def video_filepath(tmp_path: Path) -> Path:
 
 
 def test_can_save_a_video(fastapi_client: TestClient, tmp_path: Path, video_filepath: Path):
-    video_repository = LocalVideoRepository(storage_dir=tmp_path)
 
-    fastapi_client.app.dependency_overrides[get_video_repository] = lambda: video_repository  # type: ignore
+    video_repository = LocalVideoRepository(storage_dir=tmp_path)
+    frame_repository = InMemoryFrameRepository()
+    detection_model = FakeDetectionModel()
+    track_repository = InMemoryTrackRepository()
+    tracker = FakeTracker()
+
+    fastapi_client.app.dependency_overrides[_get_video_repository] = lambda: video_repository  # type: ignore
+    fastapi_client.app.dependency_overrides[_get_frame_repository] = lambda: frame_repository  # type: ignore
+    fastapi_client.app.dependency_overrides[_get_track_repository] = lambda: track_repository  # type: ignore
+    fastapi_client.app.dependency_overrides[_get_detection_model] = lambda: detection_model  # type: ignore
+    fastapi_client.app.dependency_overrides[_get_tracker] = lambda: tracker  # type: ignore
 
     with open(video_filepath, "rb") as file:
         response = fastapi_client.post("/analyze-video", files={"file": ("video.mp4", file, "video/mp4")})
@@ -37,3 +56,5 @@ def test_can_save_a_video(fastapi_client: TestClient, tmp_path: Path, video_file
     assert response.status_code == 202
     expected_video_id = IdFactory.new_video_id(VideoSource(uri="video.mp4"))
     assert response.json() == {"video_id": str(expected_video_id)}
+
+    assert len(track_repository.list_all()) == 10
--- vigil/adapters/primary/fastapi/controllers/video_analysis.py
+++ vigil/adapters/primary/fastapi/controllers/video_analysis.py
@@ -1,9 +1,9 @@
 from uuid import UUID
 
-from fastapi import APIRouter, Depends, UploadFile
+from fastapi import APIRouter, BackgroundTasks, Depends, UploadFile
 from pydantic import BaseModel
 
-from vigil.adapters.primary.fastapi.app_dependencies import get_save_video_use_case
+from vigil.adapters.primary.fastapi.app_dependencies import get_save_video_use_case, get_video_analysis_workflow
 from vigil.business_logic.models.video_source import VideoSource
 
 router = APIRouter()
@@ -18,10 +18,13 @@ class AnalyseVideoResponse(BaseModel):
 @router.post("/analyze-video", response_model=AnalyseVideoResponse, status_code=202)
 async def analyze_video(
     file: UploadFile,
+    background_tasks: BackgroundTasks,
     save_video_use_case=Depends(get_save_video_use_case),
+    video_analysis_workflow=Depends(get_video_analysis_workflow),
 ) -> AnalyseVideoResponse:
     """Analyze video post request."""
     source = VideoSource(uri=file.filename or "video")
     data = await file.read()
     video_id = save_video_use_case.execute(source=source, data=data)
+    background_tasks.add_task(video_analysis_workflow.execute, video_id)
     return AnalyseVideoResponse(video_id=video_id)
