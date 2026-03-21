5f90f0bf98e34125301f3d4650788f4e020ff87a
1bouillonbaptiste <1bouillonbaptiste@gmail.com>
2026-03-21 20:39:07 +0100

    refactor: tests uses production lifespan.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,9 @@
 import pytest
-from fastapi import FastAPI
 from starlette.testclient import TestClient
 
-from vigil.adapters.primary.fastapi.controllers import video_analysis
+from vigil.adapters.primary.fastapi.main import app
 
 
 @pytest.fixture
 def fastapi_client() -> TestClient:
-
-    app = FastAPI()
-    app.include_router(video_analysis.router)
-
     return TestClient(app)
